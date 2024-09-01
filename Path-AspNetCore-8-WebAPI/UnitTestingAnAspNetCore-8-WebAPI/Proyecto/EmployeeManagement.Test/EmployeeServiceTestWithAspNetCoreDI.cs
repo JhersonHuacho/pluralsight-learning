0c8e3406b04c92fd14406bc20bbe4079e4197f83
Francisco <jhersonhuacho@gmail.com>
2024-09-01 18:08:44 -0500

    inyección de dependencias

---
+++ Path-AspNetCore-8-WebAPI/UnitTestingAnAspNetCore-8-WebAPI/Proyecto/EmployeeManagement.Test/EmployeeServiceTestWithAspNetCoreDI.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagement.Test.Fixtures;
+
+namespace EmployeeManagement.Test;
+
+public class EmployeeServiceTestWithAspNetCoreDI : IClassFixture<EmployeeServiceWithAspNetCoreDIFixture>
+{
+	private readonly EmployeeServiceWithAspNetCoreDIFixture _employeeServiceWithAspNetCoreDIFixture;
+
+	public EmployeeServiceTestWithAspNetCoreDI(EmployeeServiceWithAspNetCoreDIFixture employeeServiceWithAspNetCoreDIFixture)
+	{
+		_employeeServiceWithAspNetCoreDIFixture = employeeServiceWithAspNetCoreDIFixture;
+	}
+
+	[Fact]
+	public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourse_WithObject()
+	{
+		// Arrange
+		var obligatoryCourse = _employeeServiceWithAspNetCoreDIFixture
+			.EmployeeManagementTestDataRepository
+			.GetCourse(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+
+		// Act
+		var internalEmployee = _employeeServiceWithAspNetCoreDIFixture
+			.EmployeeService
+			.CreateInternalEmployee("Brooklyn", "Cannon");
+
+		// Assert
+		Assert.Contains(obligatoryCourse, internalEmployee.AttendedCourses);
+	}
+}
---
+++ Path-AspNetCore-8-WebAPI/UnitTestingAnAspNetCore-8-WebAPI/Proyecto/EmployeeManagement.Test/Fixtures/EmployeeServiceWithAspNetCoreDIFixture.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement.Test.Fixtures
+{
+	public class EmployeeServiceWithAspNetCoreDIFixture : IDisposable
+	{
+		private readonly ServiceProvider _serviceProvider;
+
+		public IEmployeeManagementRepository EmployeeManagementTestDataRepository
+		{
+			get
+			{
+#pragma warning disable CS8603 // Possible null reference return.
+				return _serviceProvider.GetService<IEmployeeManagementRepository>();
+#pragma warning restore CS8603 // Possible null reference return.
+			}
+		}
+
+		public IEmployeeService EmployeeService
+		{
+			get
+			{
+				return _serviceProvider.GetService<IEmployeeService>();
+			}
+		}
+		public EmployeeServiceWithAspNetCoreDIFixture()
+        {
+			// nuestra colección de servicios
+			var services = new ServiceCollection();
+			services.AddScoped<EmployeeFactory>();
+			services.AddScoped<IEmployeeManagementRepository, EmployeeManagementRepository>();
+			services.AddScoped<IEmployeeService, EmployeeService>();
+			// lo que sigue es construir el ServiceProvider
+			// para eso se necesitamos llamar a BuildServiceProvider en la colección de servicios
+			_serviceProvider = services.BuildServiceProvider();
+
+		}
+		public void Dispose()
+		{
+			//throw new NotImplementedException();
+			// clean up the setup code, if required
+		}
+	}
+}
